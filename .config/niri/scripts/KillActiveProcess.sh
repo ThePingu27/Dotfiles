c9ac97880e5172e80bd16d8a0c184c10f51edcfb
pingu <smugcouger38@gmail.com>
2026-01-03 01:14:59 -0700

    removed wallust and unused scripts

--- .config/niri/scripts/KillActiveProcess.sh
+++ .config/niri/scripts/KillActiveProcess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Get id of an active window
-active_pid=$(hyprctl activewindow | grep -o 'pid: [0-9]*' | cut -d' ' -f2)
+active_pid=$(niri msg windows | grep -4 "focused" | grep -P "(PID: )[\d]{2,5}" | cut -c 8-13)
 # Close active window
 kill $active_pid
--- .config/niri/scripts/Refresh.sh
+++ .config/niri/scripts/Refresh.sh
@@ -1,52 +1,32 @@
 #!/bin/bash
-# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
-# Scripts for refreshing ags, waybar, rofi, swaync, wallust
-
-SCRIPTSDIR=$HOME/.config/hypr/scripts
-UserScripts=$HOME/.config/hypr/UserScripts
 
 # Define file_exists function
 file_exists() {
-    if [ -e "$1" ]; then
-        return 0  # File exists
-    else
-        return 1  # File does not exist
-    fi
+  if [ -e "$1" ]; then
+    return 0 # File exists
+  else
+    return 1 # File does not exist
+  fi
 }
 
 # Kill already running processes
-_ps=(waybar rofi swaync ags)
+_ps=(waybar)
 for _prs in "${_ps[@]}"; do
-    if pidof "${_prs}" >/dev/null; then
-        pkill "${_prs}"
-    fi
+  if pidof "${_prs}" >/dev/null; then
+    pkill "${_prs}"
+  fi
 done
 
 # added since wallust sometimes not applying
-killall -SIGUSR2 waybar 
-
-# quit ags & relaunch ags
-ags -q && ags &
+killall -SIGUSR2 waybar
 
 # some process to kill
-for pid in $(pidof waybar rofi swaync ags swaybg); do
-    kill -SIGUSR1 "$pid"
+for pid in $(pidof waybar); do
+  kill -SIGUSR1 "$pid"
 done
 
 #Restart waybar
 sleep 1
 waybar &
-
-# relaunch swaync
-sleep 0.5
-swaync > /dev/null 2>&1 &
-# reload swaync
-swaync-client --reload-config
-
-# Relaunching rainbow borders if the script exists
-sleep 1
-if file_exists "${UserScripts}/RainbowBorders.sh"; then
-    ${UserScripts}/RainbowBorders.sh &
-fi
 
 exit 0
